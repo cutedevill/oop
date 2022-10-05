0ea88d938432f73488109d01ad6a8a74b6109214
Elizaveta Boronina <85801025+cutedevill@users.noreply.github.com>
2022-10-05 19:19:36 +0700

    Add files via upload

---
+++ oop_1_2_2/Program2.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+
+namespace Threads
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Для запуска нажмите любую клавишу");
+            Console.ReadKey();
+
+            ThreadPoolWorker<int> threadPoolWorker = new ThreadPoolWorker<int>(SumNumber);
+            threadPoolWorker.Start(1000);
+
+            while (threadPoolWorker.Completed == false)
+            {
+                Console.Write('&');
+                Thread.Sleep(35);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Результат асинхронной операции = {threadPoolWorker.Result:N}");
+
+        }
+
+        private static int SumNumber(object arg)
+        {
+            int number = (int)arg;
+            int sum = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                sum += i;
+                Thread.Sleep(1);
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ oop_1_2_2/ThreadPoolWorker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    internal class ThreadPoolWorker<TResult>
+    {
+        private readonly Func<object, TResult> func;
+        private TResult result;
+
+        public ThreadPoolWorker(Func<object, TResult> func)
+        {
+            this.func = func ?? throw new ArgumentException(nameof(func));
+        }
+
+        public bool Success { get; private set; } = false;
+        public bool Completed { get; private set; } = false;
+        public Exception Exeption { get; private set; } = null;
+        public TResult Result
+        {
+            get
+            {
+                while (Completed == false)
+                {
+                    Thread.Sleep(150);
+                }
+                return Success == true && Exeption == null ? result : throw Exeption;
+            }
+        }
+
+        public void Start(object state)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadExecution), state);
+        }
+
+        private void ThreadExecution(object state)
+        {
+            try
+            {
+                func.Invoke(state);
+                Success = true;
+            }
+            catch (Exception ex)
+            {
+                Exeption = ex;
+                Success = false;
+            }
+            finally
+            {
+                Completed = true;
+            }
+        }
+    }
+}

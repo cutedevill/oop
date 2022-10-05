6fbd4d7024131bb07dcc41d35fe9e4c30b64d47f
Elizaveta Boronina <85801025+cutedevill@users.noreply.github.com>
2022-10-05 19:18:49 +0700

    Add files via upload

---
+++ oop_1_2_1/Program1.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+
+namespace Threads
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine($"Id потока метода Main - {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.WriteLine("Для запуска нажмите любую клавишу");
+            Console.ReadKey();
+
+            ThreadPoolWorker threadPoolWorker = new ThreadPoolWorker(new Action<object>(StarWriter));
+            threadPoolWorker.Start('^');
+
+            for (int i = 0; i < 120; i++)
+            {
+                Console.Write('_');
+                Thread.Sleep(50);
+            }
+
+            threadPoolWorker.Wait();
+
+            Console.WriteLine($"\nМетод Main закончил свою работу.");
+
+        }
+
+        private static void StarWriter(object arg)
+        {
+            char item = (char)arg;
+
+            for (int i = 0; i < 120; i++)
+            {
+                Console.Write(item);
+                Thread.Sleep(50);
+            }
+        }
+
+    }
+}
---
+++ oop_1_2_1/ThreadPoolWorker.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading;
+
+namespace Threads
+{
+    internal class ThreadPoolWorker
+    {
+        private readonly Action<object> action;
+
+        public ThreadPoolWorker(Action<object> action)
+        {
+            this.action = action ?? throw new ArgumentException(nameof(action));
+        }
+
+        public bool Success { get; private set; } = false;
+        public bool Completed { get; private set; } = false;
+        public Exception Exeption { get; private set; } = null;
+
+        public void Start(object state)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadExecution), state);
+        }
+
+        public void Wait()
+        {
+            while (Completed == false)
+            {
+                Thread.Sleep(150);
+            }
+
+            if(Exeption != null)
+            {
+                throw Exeption;
+            }
+        }
+
+        private void ThreadExecution(object state)
+        {
+            try
+            {
+                action.Invoke(state);
+                Success = true;
+            }
+            catch (Exception ex)
+            {
+                Exeption = ex;
+                Success = false;
+            }
+            finally
+            {
+                Completed = true;
+            }
+        }
+    }
+}

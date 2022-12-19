db232fd8b0113022cc7be3668fac5baa9ee171b8
Elizaveta Boronina <85801025+cutedevill@users.noreply.github.com>
2022-12-19 16:24:19 +0700

    Add files via upload

---
+++ oop_2/Task_Scheduler.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+
+
+
+namespace TaskSchedul
+{
+    internal class Task_Scheduler : TaskScheduler
+    {
+        private readonly LinkedList<Task> task_list = new LinkedList<Task>();
+
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return task_list;
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            Console.WriteLine($"Task {task.Id} is in queue");
+            task_list.AddLast(task);
+            ThreadPool.QueueUserWorkItem(ExecuteTasks, null);
+            AddToTheTop(task);
+            AddToTheEnd(task);
+        }
+
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            Console.WriteLine($"Synchronous task execution\r\n {task.Id}");
+
+            lock (task_list)
+            {
+                task_list.Remove(task);
+            }
+
+            return base.TryExecuteTask(task);
+        }
+
+        protected override bool TryDequeue(Task task)
+        {
+            Console.WriteLine($"Deleting the task {task.Id}");
+
+            bool res = false;
+
+            lock (task_list)
+            {
+                res = task_list.Remove(task);
+            }
+
+            if (res)
+            {
+                Console.WriteLine($"The task {task.Id} was successfully deleted");
+            }
+
+            return res;
+        }
+
+        private void ExecuteTasks(object _)
+        {
+            while (true)
+            {
+                Task task = null;
+
+                lock (task_list)
+                {
+                    if (task_list.Count == 0)
+                        break;
+
+                    task = task_list.First.Value;
+                    task_list.RemoveFirst();
+                }
+
+                if (task == null)
+                    break;
+
+                base.TryExecuteTask(task);
+            }
+        }
+
+        private void AddToTheEnd(Task task)
+        {
+            var current = task_list.FirstOrDefault(x => x.Id == task.Id);
+
+            if (current != null)
+            {
+                lock (task_list)
+                {
+                    task_list.AddLast(current);
+                    task_list.Remove(task_list.FirstOrDefault(x => x.Id == task.Id));
+                    Console.WriteLine($"The task {task.Id} was successfully added to the end");
+                }
+            }
+        }
+
+        private void AddToTheTop(Task task)
+        {
+            var current = task_list.FirstOrDefault(x => x.Id == task.Id);
+
+            if (current != null)
+            {
+                lock (task_list)
+                {
+                    task_list.Remove(task_list.FirstOrDefault(x => x.Id == task.Id));
+                    task_list.AddFirst(current);
+                    Console.WriteLine($"The task {task.Id} was successfully added to the top");
+                }
+            }
+        }
+    }
+}
+
---
+++ oop_2/main.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskSchedul;
+
+namespace oop_2
+{
+    internal class main
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"Main running with id {Thread.CurrentThread.ManagedThreadId}");
+
+            Task[] tasks = new Task[5];
+            Task_Scheduler task_scheduler = new Task_Scheduler();
+
+            QueueTaskTesting(tasks, task_scheduler);
+            TryExecuteTaskInlineTesting(tasks, task_scheduler);
+            TryDequeueTesting(tasks, task_scheduler);
+
+            try
+            {
+                Task.WaitAll(tasks);
+            }
+            catch
+            {
+                Console.WriteLine("Some tasks are canceled");
+            }
+            finally
+            {
+                Console.WriteLine("Main finished executing");
+            }
+
+            Console.ReadKey();
+        }
+
+
+        static void QueueTaskTesting(Task[] tasks, Task_Scheduler task_scheduler)
+        {
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = new Task<int>(() =>
+                {
+                    Thread.Sleep(1000 + i * 100);
+                    Console.WriteLine($"The task {Task.CurrentId} worked in the {Thread.CurrentThread.ManagedThreadId} thread");
+                    return 1;
+                });
+                tasks[i].Start(task_scheduler);
+            }
+        }
+
+        static void TryExecuteTaskInlineTesting(Task[] tasks, Task_Scheduler task_scheduler)
+        {
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = new Task<int>(() =>
+                {
+                    Thread.Sleep(1000 + i * 100);
+                    Console.WriteLine($"The task {Task.CurrentId} worked in the {Thread.CurrentThread.ManagedThreadId} thread");
+                    return 1;
+                });
+            }
+
+            foreach (var task in tasks)
+            {
+                task.Start(task_scheduler);
+                task.Wait();
+            }
+        }
+
+        static void TryDequeueTesting(Task[] tasks, Task_Scheduler task_scheduler)
+        {
+            CancellationTokenSource cancellation_token_source = new CancellationTokenSource();
+
+            CancellationToken cancellationToken = cancellation_token_source.Token;
+
+            cancellation_token_source.CancelAfter(666);
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = new Task(() =>
+                {
+                    Thread.Sleep(1000 + i * 100);
+                    Console.WriteLine($"The task {Task.CurrentId} worked in the {Thread.CurrentThread.ManagedThreadId} thread");
+                }, cancellationToken);
+            }
+
+        }
+    }
+}
